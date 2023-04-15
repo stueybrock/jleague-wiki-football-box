40b77a14221bc853788df6bc10a1d66ea090c57e
stuart-brockwell <stuart.brockwell@soapbox.co.uk>
2023-04-15 22:49:13 +0100

    Building football box array

--- index.php
+++ index.php
@@ -1,9 +1,68 @@
 <?php
 
-include ('vendor/autoload.php');
+// Crawler
+use Symfony\Component\Panther\Client;
 
-// $client = \Symfony\Component\Panther\Client::createChromeClient(__DIR__.'/../../drivers/chromedriver');
-$client = \Symfony\Component\Panther\Client::createChromeClient(__DIR__.'/drivers/chromedriver');
-$crawler = $client->request('GET', 'https://example.com');
-$fullPageHtml = $crawler->html();
-$pageH1 = $crawler->filter('h1')->text();
+require __DIR__ . '/vendor/autoload.php';
+
+$client  = Client::createChromeClient();
+$crawler = $client->request( 'GET', 'https://www.jleague.co/match/j1/2023041506/' );
+$crawler = $client->waitFor( '.player-events__body' );
+
+// Helper functions
+function get_string_between( $string, $start, $end ) {
+	$string = ' ' . $string;
+	$ini    = strpos( $string, $start );
+	if ( $ini == 0 ) {
+		return '';
+	}
+	$ini += strlen( $start );
+	$len = strpos( $string, $end, $ini ) - $ini;
+
+	return substr( $string, $ini, $len );
+}
+
+function clean( $string ) {
+	$string = str_replace( ' ', '-', $string ); // Replaces all spaces with hyphens.
+
+	return preg_replace( '/[^A-Za-z0-9\-]/', '', $string ); // Removes special chars.
+}
+
+// Round
+$round_text = $crawler->filter( '.competition-title' )->text();
+$round      = substr( $round_text, - 1 );
+
+// Date
+$date_time = $crawler->filter( '.match-date-time' )->text();
+$date_raw  = get_string_between( $date_time, ',', '·' );
+$date      = trim( ucwords( strtolower( $date_raw ) ) );
+
+// Time
+$time = trim( get_string_between( $date_time, 'OFF', 'JST' ) );
+
+// Teams
+$team_one = $crawler->filter( '.summary-teams__team--home' )->text();
+$team_two = $crawler->filter( '.summary-teams__team--away' )->text();
+
+// Score
+$score = clean( $crawler->filter( '.summary-teams__result' )->text() );
+
+$football_box = array(
+	'round'      => $round,
+	'date'       => $date,
+	'time'       => $time,
+	'team1'      => $team_one,
+	'score'      => $score,
+	'team2'      => $team_two,
+	'report'     => '',
+	'goals1'     => '',
+	'goals2'     => '',
+	'stadium'    => '',
+	'location'   => '',
+	'attendance' => '',
+	'referee'    => '',
+	'result'     => '',
+);
+
+var_dump( $football_box );
+
